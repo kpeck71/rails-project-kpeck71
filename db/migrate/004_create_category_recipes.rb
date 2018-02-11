f142d30fa2b62038305d06c3b8e8485f01e42ea5
kpeck71 <kpeck71@gmail.com>
2018-02-11 15:37:04 -0500

    updated migrations with ActiveRecord::Migration[4.2]

--- db/migrate/004_create_category_recipes.rb
+++ db/migrate/004_create_category_recipes.rb
@@ -1,4 +1,4 @@
-class CreateCategoryRecipes < ActiveRecord::Migration
+class CreateCategoryRecipes < ActiveRecord::Migration[4.2]
   def change
     create_table :category_recipes do |t|
       t.integer :category_id

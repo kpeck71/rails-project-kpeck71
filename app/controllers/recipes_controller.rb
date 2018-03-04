67e66dac0342d9e15d29fc2c72b88e33f4c739b9
kpeck71 <kpeck71@gmail.com>
2018-03-04 12:03:42 -0500

    ingredient attributes on new recipe form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,18 @@
-class RecipesController < AppicationController
+class RecipesController < ApplicationController
+
+  def new
+    @recipe = Recipe.new
+    10.times { @recipe.ingredients.build}
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name, ingredient_attributes: [:id, :name, :description])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
 
   post 'recipes/:id/cooked', to: 'recipes#cooked', as: 'cooked'
 
+  resources :recipes
+  resources :categories
+  resources :users
+
+  get '/signup', to: 'users#new'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
 end

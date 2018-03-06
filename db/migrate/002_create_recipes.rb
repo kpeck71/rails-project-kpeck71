fb3d222cd7f55276f36c933c9a6791d729fe87b8
kpeck71 <kpeck71@gmail.com>
2018-03-05 19:29:15 -0500

    drop tables, update with recipe_ingredient relationship

--- db/migrate/002_create_recipes.rb
+++ db/migrate/002_create_recipes.rb
@@ -2,7 +2,6 @@ class CreateRecipes < ActiveRecord::Migration[4.2]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.string :ingredients
       t.integer :user_id
     end
   end

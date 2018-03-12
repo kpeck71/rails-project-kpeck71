42542f73d921292fdb3ea636ca42e4261472c9fc
kpeck71 <kpeck71@gmail.com>
2018-03-11 20:10:45 -0400

    add pry; update customer writer for ingredients_attributes; change ingredient to plural where needed

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,9 @@
 class RecipesController < ApplicationController
 
   def new
+    #giving my form an AR instance to wrap around so my form knows which model I'm building this form for
     @recipe = Recipe.new
-    5.times { @recipe.ingredients.build}
+    @recipe.ingredients.build
   end
 
   def create
@@ -13,7 +14,7 @@ class RecipesController < ApplicationController
 
   private
     def recipe_params
-      params.require(:recipe).permit(:name, category_ids:[], ingredient_attributes: [:id, :name, :description])
+      params.require(:recipe).permit(:name, :category_ids => [], :ingredients_attributes =>[:name, :description])
     end
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,11 +6,37 @@ class Recipe < ActiveRecord::Base
   has_many :categories, through: :category_recipes
   validates :ingredients, presence: true
 
-  def ingredient_attributes=(ingredient_attribute)
-    ingredient_attributes.values.each do |ingredient_attribute|
-      recipe = Recipe.find_or_create_by(ingredient_attribute)
-      recipe.ingredients << ingredient
+  def ingredients_attributes=(ingredients_attributes)
+    ingredients_attributes.values.each do |ingredient_attribute|
+       ingredient = Ingredient.find_or_create_by(ingredient_attribute)
+       self.ingredients.build(ingredient_attribute)
+       #self.ingredients.build(ingredient_attribute)
     end
   end
+#
+# On this form you would have a series of fields named
+# recipe[ingredient_attributes][0][name] and
+# recipe[ingredient_attributes][0][description] which would write
+# to the recipe model through a method ingredient_attributes=.
+# This method cannot be provided via the accepts_nested_attributes_for
+# macro because the custom writer would be responsible for finding or
+# creating a recipe by name and then creating the row in the join model
+# recipe_ingredients with the recipe_id, the ingredient_id, and the
+# description from the form.
 
 end
+
+#
+# class Post < ActiveRecord::Base
+#   has_many :post_categories
+#   has_many :categories, through: :post_categories
+#
+#
+#   def categories_attributes=(category_attributes)
+#     category_attributes.values.each do |category_attribute|
+#       category = Category.find_or_create_by(category_attribute)
+#       self.post_categories.build(category: category)
+#     end
+#   end
+#
+# end

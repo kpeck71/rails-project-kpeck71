e896b393b6370c855f998aec84bbf4e5010feb02
kpeck71 <kpeck71@gmail.com>
2018-03-13 22:03:22 -0400

    fix hidden user_id in form, caused @recipe to be invalid

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,9 +9,10 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     if params[:recipe][:categories]
-      category = Category.find_or_create_by(name: params[:recipe][:categories][:name])
+      category = Category.find_or_create_by(name: params[:recipe][:categories])
       @recipe.categories << category
     end
+    raise @recipe.inspect
     if @recipe.save
       redirect_to recipe_path(@recipe)
     else
@@ -21,7 +22,7 @@ class RecipesController < ApplicationController
 
   private
     def recipe_params
-      params.require(:recipe).permit(:name, :category_ids => [], :ingredients_attributes =>[:name, :description])
+      params.require(:recipe).permit(:name, :user_id, :category_ids => [], :ingredients_attributes =>[:name, :description])
     end
 
 end
